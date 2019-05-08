e5348a11cf827518c13e4439d801163c140bb222
Santiago Liscano <35749289+SantiagoLiscano@users.noreply.github.com>
2019-05-08 17:30:52 -0400

    Update cleverPlan.js

--- neededFiles/cleverPlan.js
+++ neededFiles/cleverPlan.js
@@ -3,7 +3,7 @@ var dig = document.getElementById('dig');
 var resul = document.getElementById('resultado');
 var back = document.getElementById('back');
 var turn = 1;
-var tasa = {digitel: 4.99989, movistar: 5}; //REPRESENTA LAS TASAS DE BSF/MB.
+var tasa = {digitel: 4.99, movistar: 5}; //REPRESENTA LAS TASAS DE BSF/MB.
 var first = true;
 var input = document.getElementById('valor');
 
